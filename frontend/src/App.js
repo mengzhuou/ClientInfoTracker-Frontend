acb1ca2d0ea7c1424a86457f737f03a7de5f23fc
mengzhuou <mou30@gatech.edu>
2024-12-16 20:43:42 -0500

    Hide the top bar for 404

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import EnterCode from "./components/Pages/EnterCode/EnterCode";
 import MainPage from "./components/Pages/MainPage/MainPage";
 import CreateClient from "./components/Pages/CreateClient";
@@ -8,19 +8,36 @@ import TopNavBar from "./components/Functions/TopNavBar/TopNavBar";
 import Draft from './components/Pages/DraftPage/DraftPage';
 import PageNotFound from './components/Pages/ErrorPage/PageNotFound/PageNotFound';
 
+// conditionally render TopNavBar
+const Layout = ({ children }) => {
+  const location = useLocation();
+
+  // Hide TopNavBar only when rendering PageNotFound
+  const is404 = location.pathname !== "/" && !["/MainPage", "/draft", "/create-client", "/edit-existing-client"].includes(location.pathname);
+
+  console.log("children:" , children)
+  return (
+    <>
+      {!is404 && <TopNavBar />}
+      {children}
+    </>
+  );
+};
+
 class App extends Component {
   render() {
     return (
       <Router>
-        <TopNavBar/>
-        <Routes>
-          <Route path="/" element={<EnterCode/>} />
-          <Route path="/MainPage" element={<MainPage/>}/>
-          <Route path="/draft" element={<Draft/>} />
-          <Route path="/create-client" element={<CreateClient />} />
-          <Route path="/edit-existing-client" element={<EditExistingClient />} />
-          <Route path="*" element={<PageNotFound />} /> {/* Catch-all route for 404 */}
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<EnterCode />} />
+            <Route path="/MainPage" element={<MainPage />} />
+            <Route path="/draft" element={<Draft />} />
+            <Route path="/create-client" element={<CreateClient />} />
+            <Route path="/edit-existing-client" element={<EditExistingClient />} />
+            <Route path="*" element={<PageNotFound />} /> {/* Catch-all route */}
+          </Routes>
+        </Layout>
       </Router>
     );
   }
